cfd28787a67c71540d792ee0441a6ba3561750c0
Elaidzha1940 <eli080293@gmail.com>
2023-04-23 02:35:36 +0700

    ViewBuilder / DatePicker

---
+++  DatePicker/ DatePicker/_DatePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DatePickerApp.swift
+//   DatePicker
+//
+//  Created by Elaidzha Shchukin on 23.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct _DatePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
