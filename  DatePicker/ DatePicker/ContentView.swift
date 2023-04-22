3a19f0f34e8e8977fc496e1608736b0e831fabd5
Elaidzha1940 <eli080293@gmail.com>
2023-04-23 02:44:05 +0700

    ViewBuilder / DatePicker

---  DatePicker/ DatePicker/ContentView.swift
+++  DatePicker/ DatePicker/ContentView.swift
@@ -1,21 +1,23 @@
+//  /*
 //
-//  ContentView.swift
-//   DatePicker
+//  Project: DatePicker
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 23.04.2023
 //
-//  Created by Elaidzha Shchukin on 23.04.2023.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack(spacing: 12) {
+            
+            UserView(title: "swiftUI", iconName: "")
+            UserView(title: "swiftUI1", iconName: "")
+            UserView(title: "swiftUI2", iconName: "")
         }
-        .padding()
     }
 }
 
@@ -24,3 +26,27 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct UserView: View {
+    
+    let title: String
+    let iconName: String
+    
+    var body: some View {
+        
+        Button {
+            print()
+        } label: {
+            Text("iOS")
+                .padding(.horizontal, 8)
+            Spacer()
+            //Image("")
+                .padding(.trailing)
+        }
+        .frame(width: 400, height: 50)
+        .background(Color.red)
+        .foregroundColor(Color.brown)
+        .cornerRadius(14)
+    }
+}
+
